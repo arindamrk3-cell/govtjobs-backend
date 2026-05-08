e817ae8f9b555a0fd7d634d26f8177b7699cd7b0
Arindam Sarkar <arindamrk3@gmail.com>
2026-05-08 18:29:15 +0530

    Comment out fetch import in pushService.js
    Commented out the fetch import statement.

--- services/pushService.js
+++ services/pushService.js
@@ -1,13 +1,14 @@
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
 
 const sendPush = async (tokens, title, body) => {
-  try {
+  
     const messages = tokens.map(token => ({
       to: token,
       sound: "default",
       title,
       body
     }));
+  try {
 
     const res = await fetch("https://exp.host/--/api/v2/push/send", {
       method: "POST",
